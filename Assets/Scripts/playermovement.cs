88312b1b17a81189523d02686522df461ed509ee
Adaia León <adaialeoncts@gmail.com>
2022-09-21 10:53:48 +0200

    movimiento 1

--- Assets/Scripts/playermovement.cs
+++ Assets/Scripts/playermovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    //VIDEO
+    private Rigidbody2D rb;
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();       
+
     }
 
-    // Update is called once per frame
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + new Vector2 (1f, 0f) * Time.deltaTime);
+    }
+
+    //Movimiento del personaje
+    public float speed = 5.5f; 
+
+    //Almacenamiento de imputs, movimiento horizontal
+    private float horizontal;
+
+    //Variable para acceder al transform
+    private Transform playerTransform;
+
+    //Se ejecuta en el primer frame
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+
+    //Se ejecuta una vez por frame
     void Update()
     {
-        
-    }
+        horizontal = Input.GetAxisRaw("Horizontal");        
+
+        //Acceder a la posicion del transform (se puede escribir de varias maneras)
+        //playerTransform.position += new Vector3 (horizontal * speed * Time.deltaTime, 0, 0);
+        //playerTransform.position += new Vector3 (1, 0, 0) * horizontal * speed * Time.deltaTime;
+        //playerTransform.Translate(Vector3.right * horizontal * speed * Time.deltaTime, Space.World);
+        playerTransform.Translate(Vector2.right * horizontal * speed * Time.deltaTime, Space.World);
+
+
+    }   
 }
